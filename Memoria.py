318098c6f47d0726de98d8220199ab1753d792d5
akemi323 <leticiaakemi123456@gmail.com>
2025-11-24 18:13:10 -0300

    memoria

--- Memoria.py
+++ Memoria.py
@@ -1,18 +1,13 @@
-import random
+from random import randint
 
 class MemoriaPrincipal:
     def __init__(self, tam=50):
         self.tamanho = tam
-        self.celulas = [random.randint(1, 4) for _ in range(tam)] #Vi isso aqui na net mas não sei se é o melhor jeito
+        self.celulas = [randint(1, 4) for _ in range(tam)] #Vi isso aqui na net mas não sei se é o melhor jeito
     
     def ler(self, endereco):
-        self.verifica_endereco(endereco)
         return self.celulas[endereco]
     
     def escrever(self, endereco, valor):
-        self.verifica_endereco(endereco)
         self.celulas[endereco] = valor
-    
-    def verifica_endereco(self, endereco):
-       pass
     
